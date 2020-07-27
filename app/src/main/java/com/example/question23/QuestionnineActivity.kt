694c9045a89b6db1b8cf85f7ac534caa8f9706be
zhanysch <zhanysch@gmail.com>
2020-07-27 19:46:49 +0600

    question23

---
+++ app/src/main/java/com/example/question23/QuestionnineActivity.kt
@@ -0,0 +1,11 @@
+package com.example.question23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuestionnineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
